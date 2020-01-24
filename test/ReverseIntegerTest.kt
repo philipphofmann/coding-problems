0d8fb2efad55c38c7eaa44f69de291184642610f
Philipp Hofmann <ph0hofmann@gmail.com>
2020-01-24 16:45:17 +0100

    Rename ReverseIntegerTest test methods

--- test/ReverseIntegerTest.kt
+++ test/ReverseIntegerTest.kt
@@ -4,29 +4,29 @@ import kotlin.test.assertEquals
 internal class ReverseIntegerTest {
 
     @Test
-    fun twoDigits() {
+    fun `two digits`() {
         val actual = ReverseInteger.reverse(12)
 
         assertEquals(21, actual)
     }
 
     @Test
-    fun twoMinusDigits() {
+    fun `two negative digits`() {
         val actual = ReverseInteger.reverse(-12)
 
         assertEquals(-21, actual)
     }
 
     @Test
-    fun tenMinusDigits() {
+    fun `ten negative digits`() {
         val actual = ReverseInteger.reverse(-123898274)
 
         assertEquals(-472898321, actual)
     }
 
     @Test
-    fun biggerThanIntMaxValue() {
-        val actual = ReverseInteger.reverse(-1238982742)
+    fun `bigger than Int Max value`() {
+        val actual = ReverseInteger.reverse(1238982742)
 
         assertEquals(0, actual)
     }
