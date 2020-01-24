8f79adbb2c95cee4d35110b82cb6deffa2429470
Philipp Hofmann <ph0hofmann@gmail.com>
2020-01-24 16:51:53 +0100

    Change missing number to a top level function

--- test/MissingNumberTest.kt
+++ test/MissingNumberTest.kt
@@ -4,16 +4,16 @@ import kotlin.test.assertEquals
 internal class MissingNumberTest {
 
     @Test
-    fun findMissingNumber() {
-        val actual = MissingNumber.findMissingNumber(arrayOf(0, 1, 3))
+    fun `second number is missing`() {
+        val actual = findMissingNumber(arrayOf(0, 1, 3))
 
         assertEquals(2, actual)
     }
 
     @Test
-    fun findMissingNumbera() {
-        val actual = MissingNumber.findMissingNumber(arrayOf(10, 11, 12, 13))
+    fun `five digits`() {
+        val actual = findMissingNumber(arrayOf(0, 1, 2, 3, 5))
 
-        assertEquals(2, actual)
+        assertEquals(4, actual)
     }
 }
